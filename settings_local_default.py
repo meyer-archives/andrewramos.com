17506060c30a0ab02c6e0d0066a00697cba77962
Mike Meyer (gmail) <github@meyer.fm>
2010-08-11 13:01:05 -0400

    Bam

---
+++ settings_local_default.py
@@ -0,0 +1,27 @@
+DATABASES = {
+	'default': {
+		'ENGINE':	'django.db.backends.sqlite3',
+		'NAME':		'data.db',
+		'USER':		'',
+		'PASSWORD':	'',
+		'HOST':		'',
+		'PORT':		'',
+	}
+}
+
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE':	'django.db.backends.postgresql_psycopg2',
+# 		'NAME':		'dbname',
+# 		'USER':		'dbuser',
+# 		'PASSWORD':	'dbpass',
+# 		'HOST':		'localhost',
+# 		'PORT':		'5432',
+# 	}
+# }
+
+MEDIA_URL = '/media/'
+ADMIN_MEDIA_PREFIX = '/media/admin/'
+
+#DEBUG = True
+#TEMPLATE_DEBUG = DEBUG
