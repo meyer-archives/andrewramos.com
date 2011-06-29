712bae5bd8a320534faa3cf5210a13f9ddd6e10c
Mike Meyer (gmail) <github@meyer.fm>
2011-06-28 17:40:36 -0700

    iOS 5 magics added

--- apps/mobile/middleware.py
+++ apps/mobile/middleware.py
@@ -2,52 +2,56 @@ from django.conf import settings
 from useragents import search_strings
 
 class MobileMiddleware(object):
-    def process_request(self, request):
-        """Mobile device detection!"""
+	def process_request(self, request):
+		"""Mobile device detection!"""
 
-        request.enable_mobile = False
-        request.mobile = False
-        request.mobile_safari = False
-        request.iphone = False
-        request.ipad = False
-        request.mobile_device = False
+		request.enable_mobile = False
+		request.mobile = False
+		request.mobile_safari = False
+		request.iphone = False
+		request.ipad = False
+		request.mobile_device = False
+		request.ios5 = False
 
 #         if request.GET.has_key('mobile'):
 #             request.session['enable_mobile'] = request.GET.get('mobile')
-        
-        request.enable_mobile = True #request.session.get('enable_mobile', 'false').lower() == 'true'
-        
+
+		request.enable_mobile = True #request.session.get('enable_mobile', 'false').lower() == 'true'
+		
 #        print('request.enable_mobile: %s' % request.enable_mobile)
 
-        # Parse useragent, check for mobile user
-        if request.enable_mobile:
+		# Parse useragent, check for mobile user
+		if request.enable_mobile:
 
-            # Opera Mini headers
-            if request.META.has_key("HTTP_X_OPERAMINI_FEATURES"):
-                request.mobile = True
+			# Opera Mini headers
+			if request.META.has_key("HTTP_X_OPERAMINI_FEATURES"):
+				request.mobile = True
 
-            # Useragent parsing
-            elif request.META.has_key("HTTP_USER_AGENT"):
-                s = request.META["HTTP_USER_AGENT"].lower()
+			# Useragent parsing
+			elif request.META.has_key("HTTP_USER_AGENT"):
+				s = request.META["HTTP_USER_AGENT"].lower()
 
-                if "iphone" in s or "ipod" in s:
-                    request.iphone = "iphone" in s or "ipod" in s
-                    request.mobile_safari = True
-                    request.mobile = True
-                elif "ipad" in s:
-                    request.ipad = True
-                    request.mobile_safari = True
-                    request.mobile = False
-                else:
-                    for ua in search_strings:
-                        if ua in s:
-                            request.mobile = True
+				if "iphone" in s or "ipod" in s or 'ipad' in s:
+					if 'ipad' in s:
+						request.ipad = True
+					else:
+						request.iphone = True
+					request.mobile_safari = True
+					request.mobile = True
 
-        if request.mobile:
-            if settings.TEMPLATE_DIRS != settings.MOBILE_TEMPLATE_DIRS:
-                settings.TEMPLATE_DIRS = settings.MOBILE_TEMPLATE_DIRS
-        else:
-            if settings.TEMPLATE_DIRS != settings.SITE_TEMPLATE_DIRS:
-                settings.TEMPLATE_DIRS = settings.SITE_TEMPLATE_DIRS
-                
-        return None
+					# iOS 5 check
+					if 'OS 5_' in s:
+						request.ios5 = True
+				else:
+					for ua in search_strings:
+						if ua in s:
+							request.mobile = True
+
+		if request.mobile:
+			if settings.TEMPLATE_DIRS != settings.MOBILE_TEMPLATE_DIRS:
+				settings.TEMPLATE_DIRS = settings.MOBILE_TEMPLATE_DIRS
+		else:
+			if settings.TEMPLATE_DIRS != settings.SITE_TEMPLATE_DIRS:
+				settings.TEMPLATE_DIRS = settings.SITE_TEMPLATE_DIRS
+				
+		return None
--- fabfile.py
+++ fabfile.py
@@ -6,53 +6,9 @@ import settings
 management.setup_environ(settings)
 
 from fabric.api import *
-# This will import every command, you may need to get more selective if
-# you aren't using all of the stuff we do.
-# For example:
-#from fabtastic.fabric.commands.c_common import *
-#from fabtastic.fabric.commands.c_git import git_pull
-from fabtastic.fabric.commands import *
-from fabtastic.fabric.commands.c_gunicorn import gunicorn_restart_workers
-
-"""
-Here are some deployment related settings. These can be pulled from your
-settings.py if you'd prefer. We keep strictly deployment-related stuff in
-our fabfile.py, but you don't have to.
-"""
-
-env.REMOTE_CODEBASE_PATH = '/www/django/andrewramos'
-env.PIP_REQUIREMENTS_PATH = 'requirements.txt'
-env.REMOTE_VIRTUALENV_NAME = 'looklab'
-
-env.GUNICORN_PID_PATH = os.path.join('/www/', 'gunicorn.pid')
-
-def dev():
-	"""
-	Sets env.hosts to the sole staging server. No roledefs means that all
-	deployment tasks get ran on every member of env.hosts.
-	"""
-	env.hosts = ['webdev@andrewramos.com:8675']
 
 def update_db():
-	run('cd /www/django/looklab/ && workon looklab && ./manage.py ft_dump_db db_latest.bz2')
-	get('/www/django/looklab/db_latest.bz2','db_latest.bz2')
-	local('./manage.py ft_restore_db db_latest.bz2')
+	local('gondor sqldump primary -- | psql -U andrewramos -d andrewramos')
 
 def deploy():
-	"""
-	Full git deployment. Migrations, reloading gunicorn.
-	"""
-	git_pull()
-	south_migrate()
-#	 gunicorn_restart_workers()
-	sudo('/etc/init.d/supervisord restart')
-#	flush_cache()
-#	 local('./manage.py syncmedia')
-#	 mediasync_syncmedia()
-
-def deploy_soft():
-	"""
-	Just checkout the latest source, don't reload.
-	"""
-	git_pull()
-	print("--- Soft Deployment complete. ---")
+	local('gondor deploy primary master')
