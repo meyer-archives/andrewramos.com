57065f6f9448785fc0f095e2c5b0a350d20bf756
Mike Meyer (gmail) <github@meyer.fm>
2013-08-13 10:27:39 -0700

    FML PT. 1
    Apparently “media/portfolio/Adam.tif” is too big for Github.

---
+++ deploy_to_s3/-/j/site.js
@@ -0,0 +1,84 @@
+jQuery(function($) {
+$.fn.extend({
+	slideshowify: function(options) {
+		var settings = $.extend({}, $.fn.slideshowify.defaults, options);
+		return this.each(
+			function(){
+				var $g = $(this);
+				var o = $.metadata ? $.extend({}, settings, $g.metadata()) : settings;
+				var $slides = $g.addClass("slideshow-active").find("ul>li").hide();
+				var $clickers = $("<div class='slideshow-links'></div>");
+				var currentSlide;
+				var animating = false;
+				var currentClicker;
+
+				var $next = $("<a href='#' class='next-slide'>Next slide</a>").click(function(){
+					if( currentClicker.length && currentClicker.next().length )
+						currentClicker.next().click();
+					return false;
+				});
+				var $prev = $("<a href='#' class='prev-slide'>Previous slide</a>").click(function(){
+					if( currentClicker.length && currentClicker.prev().length )
+						currentClicker.prev().click();
+					return false;
+				});
+
+				$slides.each(function(i){
+					var $slide = $(this);
+					var $clicker = $("<a href='#slide-"+(i+1)+"'>Show slide "+(i+1)+"</a>").click(function(){
+						if( animating || currentSlide == $slide.index() )
+							return false;
+						animating = true;
+						currentClicker = $clicker;
+						if( $clicker.next().length ){
+							$next.show();
+						} else {
+							$next.hide();
+						}
+						if( $clicker.prev().length ){
+							$prev.show();
+						} else {
+							$prev.hide();
+						}
+						$clicker.addClass("current").siblings().removeClass("current");
+						$slide.css({zIndex:100}).fadeIn(250,function(){
+							$slide.css({zIndex:50}).siblings().hide()
+							animating = false;
+						});
+						currentSlide = $slide.index();
+						return false;
+					}).appendTo($clickers);
+				});
+
+				if( $slides.length > 1 )
+					$g.append($clickers).append($next).append($prev);
+				$clickers.find("a:first").click();
+
+				return this;
+			}
+		);
+	}
+});
+$.fn.slideshowify.defaults = {
+	transition: 'slide' // Not in use yet
+};
+});
+
+$(document).ready(function() {
+	var $btt = $("a#back-to-top").hide();
+	var timer;
+	function btt_toggle(){
+		if( $btt.length && $("#content-inner").height() > $(document).height() ){
+			$btt.fadeIn(400);
+		} else {
+			$btt.fadeOut(400);
+		}
+	}
+	btt_toggle();
+	$(window).resize(function(){
+		clearTimeout(timer);
+		timer = setTimeout(btt_toggle,750);
+	})
+	// What a dumb name.
+	$(".slideshow").slideshowify();
+});
